9bdfc5e1022d9a8ed44aaa4fee2658424bf13476
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-06-11 11:41:02 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,24 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  return array.sort
 end
 
 def using_reverse(array)
-
+  return array.reverse
 end
 
 def using_first(array)
-
+  return array[0]
 end
 
 def using_last(array)
-
+    return array [-1]
 end
 
 def using_size(array)
+  return array.size
 
 end
